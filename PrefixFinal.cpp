f870ff20deb7be7211144631692b1367d4bc9187
Srikant <96463545+Srikant33@users.noreply.github.com>
2022-01-16 00:27:18 -0500

    Add files via upload

---
+++ PrefixFinal.cpp
@@ -0,0 +1,125 @@
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+
+// Simply determine if character is one of the four standard operators.
+bool isOperator(char character) {
+    if (character == '+' || character == '-' || character == '*' || character == '/' || character  == '='||  character  == '^') {
+        return true;
+    }
+    return false;
+}
+
+
+// If the character is not an operator or a parenthesis, then it is assumed to be an operand.
+bool isOperand(char character) {
+    if (!isOperator(character) && character != '(' && character != ')') {
+        return true;
+    }
+    return false;
+}
+
+
+// Compare operator precedence of main operators.
+// Return 0 if equal, -1 if op2 is less than op1, and 1 if op2 is greater than op1.
+int compareOperators(char op1, char op2) {
+    if ((op1 == '*' || op1 == '/'||  op1 == '^') && (op2 == '+' || op2 == '-')) { return -1; }
+    else if ((op1 == '+' || op1 == '-' ) && (op2 == '*' || op2 == '/')) { return 1; }
+    return 0;
+}
+
+//string strrev(string str)
+/*{
+    int n = str.length();
+ 
+    // Swap character starting from two
+    // corners
+    for (int i=0; i<n/2; i++)
+       swap(str[i], str[n-i-1]);
+       
+       return str;
+}
+*/
+int main()
+{
+    // Empty character stack and blank postfix string.
+    stack<char> opStack;
+    string postFixString = "";
+	
+    char input[100];
+
+    // Collect input
+    cout << "Enter an expression: ";
+    cin >> input;
+	
+	int n;
+	while (input[n]!='\0')
+	{	n++;
+	}
+	
+	for (int i=0; i<n/2; i++)
+       swap(input[i], input[n-i-1]);
+    
+	for(int i=0; i<n; i++)
+	{	if (input[i]=='(')
+		{	input[i]=')';	}
+		else if (input[i]==')')
+		{	input[i]='('; 	} 
+	}    
+       
+      // cout<<input<<endl;
+	//input = strrev(input);
+    // Get a pointer to our character array.
+    char *cPtr = input;
+
+    // Loop through the array (one character at a time) until we reach the end of the string.
+    while (*cPtr != '\0') {
+        // If operand, simply add it to our postfix string.
+        // If it is an operator, pop operators off our stack until it is empty, an open parenthesis or an operator with less than or equal precedence.
+        if (isOperand(*cPtr)) { postFixString += *cPtr; }
+        else if (isOperator(*cPtr)) 
+		{
+            while (!opStack.empty() && opStack.top() != '(' && compareOperators(opStack.top(),*cPtr) < 0) 
+		    {
+                postFixString += opStack.top();
+                opStack.pop();
+            }
+            opStack.push(*cPtr);
+        }
+        // Simply push all open parenthesis onto our stack
+        // When we reach a closing one, start popping off operators until we run into the opening parenthesis.
+        else if (*cPtr == '(') { opStack.push(*cPtr); } 
+        else if (*cPtr == ')') {
+            while (!opStack.empty()) {
+                if (opStack.top() == '(') { opStack.pop(); break; }
+                postFixString += opStack.top();
+                opStack.pop();
+            }
+        }
+
+        // Advance our pointer to next character in string.
+        cPtr++;
+    }
+
+    // After the input expression has been ran through, if there is any remaining operators left on the stack
+    // pop them off and put them onto the postfix string.
+    while (!opStack.empty()) {
+        postFixString += opStack.top();
+        opStack.pop();
+    }
+
+	int m;
+	while (postFixString[m]!='\0')
+	{	m++;
+	}
+	//cout<<postFixString<<endl;
+	for (int i=0; i<m/2; i++)
+       swap(postFixString[i], postFixString[m-i-1]);
+//	postFixString=strrev(postFixString);
+    // Show the postfix string at the end.
+    cout << "Prefix is: " << postFixString << endl;
+    return 0;
+}
